3c12ac7bea05081f48c0dc3751324eb0d24672dd
AJ0148 <155866630+AJ0148@users.noreply.github.com>
2024-06-18 22:08:15 -0400

    added experimental code

---
+++ js/rng.js
@@ -0,0 +1,10 @@
+const num = document.getElementById('number');
+const btn = document.getElementById('gen');
+
+const randomNum = () => {
+  return Math.floor(Math.random() * 1000);
+};
+
+btn.addEventListener('click', () => {
+  num.innerHTML = randomNum();
+});
